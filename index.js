8524052422f0b7328714da259331ec55448e7487
NieamulKabir <kabir000079@gmail.com>
2023-12-25 13:10:48 +0600

    cart get update

--- index.js
+++ index.js
@@ -36,6 +36,15 @@ async function run() {
     });
 
     //cart collection
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        res.send([]);
+      }
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
     app.post("/carts", async (req, res) => {
       const item = req.body;
       console.log(item);
