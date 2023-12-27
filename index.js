f249c2ec57432764ac297a2711a81777cb3823f3
NieamulKabir <kabir000079@gmail.com>
2023-12-27 20:04:05 +0600

    update

--- index.js
+++ index.js
@@ -33,6 +33,11 @@ async function run() {
     //user api
     app.post("/users", async (req, res) => {
       const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exists" });
+      }
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
