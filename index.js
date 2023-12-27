371b60d754b67d35d1b3476f96f5067cd4c8c4d7
NieamulKabir <kabir000079@gmail.com>
2023-12-27 18:55:51 +0600

    create user collection

--- index.js
+++ index.js
@@ -25,10 +25,21 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const usersCollection = client.db("bistroDb").collection("users");
     const menuCollection = client.db("bistroDb").collection("menu");
     const reviewsCollection = client.db("bistroDb").collection("reviews");
     const cartCollection = client.db("bistroDb").collection("carts");
 
+    //user api
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
     //menu collection
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
