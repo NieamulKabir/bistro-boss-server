bf3501fbd55c91e6595fba1abd97a3b60d07b2a5
NieamulKabir <kabir000079@gmail.com>
2023-12-30 01:10:27 +0600

    verify admin

--- index.js
+++ index.js
@@ -153,7 +153,7 @@ async function run() {
       res.send(result);
     });
 
-    app.post("/menu", async (req, res) => {
+    app.post("/menu", verifyJWT, verifyAdmin, async (req, res) => {
       const newItem = req.body;
       const result = await menuCollection.insertOne(newItem);
       res.send(result);
